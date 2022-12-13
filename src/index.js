0d0a06705e680332bad360f6941fe46b096ac674
Albishhh <117079550+Albishhh@users.noreply.github.com>
2022-12-13 14:53:25 +0100

    app4

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import SongWeb from './componentes/SONGWEB/SongWeb';
-import Discover from './Discover';
-import Join from './Join';
+import Discover from './componentes/DISCOVER/Discover';
+import Join from './componentes/JOIN/Join';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
